417ad443f0da7382aa6cc352d3b897100b6ffb47
Y4zidd <yazidistiqlaladhy@student.telkomuniversity.ac.id>
2024-10-08 15:03:15 +0700

    add jurnal1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // cetak nama
+        System.out.println("Nama: Yazid Istiqlal Adhy Fadhillah!");
+        System.out.println("NIM: 607062430005");
+        System.out.println("Nama: D3IF-48-02");
+        }
+    }
